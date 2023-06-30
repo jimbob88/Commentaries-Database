74d2a4d1e44aea88810b015ecb5c1e99e44c0f1b
James Blackburn <blackburnfjames@gmail.com>
2023-06-30 13:47:50 +0100

    Improved quality by exploding the process_toml function

--- compile_data.py
+++ compile_data.py
@@ -5,8 +5,10 @@ import sqlite3
 import csv
 from pathlib import Path
 import rtoml
+from tqdm import tqdm
 import uuid
-from typing import NamedTuple, Dict
+from typing import List, NamedTuple, Dict, Union
+from collections.abc import Iterator, Iterable
 import re
 import argparse
 
@@ -54,83 +56,109 @@ def encode_chapter_verse(chapter: int, verse: int) -> int:
     return (chapter * 1000000) + verse
 
 
-def process_toml():
-    file_list = [path for path in Path.cwd().glob('**/*') if path.is_file() and path.suffix == '.toml']
+class PathMetadata(NamedTuple):
+    father_name: str
+    bible_book_name: str
+    bible_verse_range: VerseRange
 
+
+def path_to_metadata(toml_path: Path) -> PathMetadata:
+    father_name = toml_path.parent.name
+
+    fn_pieces = toml_path.stem.split(" ")
+    book_name = " ".join(fn_pieces[:-1])
+    verse_range = string_to_verse_range(fn_pieces[-1])
+
+    return PathMetadata(father_name, book_name, verse_range)
+
+
+def commentary_date(father_name: str, father_meta_data: Dict, commentary: Dict, default=9999999) -> int:
+    """Calculates the date of the commentary"""
+    date = default
+    if father_name in father_meta_data:
+        date = father_meta_data[father_name]['default_year']
+    date = commentary.get('time', date)
+    return date
+
+
+class Commentary(NamedTuple):
+    filename: str
+    father_name: str
+    bible_book_name: str
+    bible_verse_range: VerseRange
+    source_url: str
+    source_title: str
+    date: int
+    append_to_author_name: str
+    txt: str
+
+
+def process_toml_file(toml_path: Path, father_meta_data) -> Union[Iterator[Commentary], None]:
+    """Extracts all the commentaries from a toml file"""
+    path_metadata = path_to_metadata(toml_path)
+
+    toml_str = toml_path.read_text(encoding='utf-8')
+    try:
+        toml_obj = rtoml.load(toml_str)
+    except rtoml.TomlParsingError as e:
+        print(f"{toml_path} failed because:", e)
+        return iter(())
+
+    for commentary in toml_obj['commentary']:
+        source_url = commentary['sources'][0]['url'] if 'sources' in commentary else ""
+        source_title = commentary['sources'][0]['title'] if 'sources' in commentary else ""
+        date = commentary_date(path_metadata.father_name, father_meta_data, commentary)
+        append_to_author_name = commentary.get('append_to_author_name', '')
+
+        yield Commentary(
+            filename=toml_path.name,
+            father_name=path_metadata.father_name,
+            bible_book_name=path_metadata.bible_book_name,
+            bible_verse_range=path_metadata.bible_verse_range,
+            source_url=source_url,
+            source_title=source_title,
+            date=date,
+            append_to_author_name=append_to_author_name,
+            txt=commentary['quote']
+        )
+
+
+def commentary_to_row(commentary: Commentary) -> List:
+    location_start = encode_chapter_verse(commentary.bible_verse_range.start_chapter, commentary.bible_verse_range.start_verse)
+    location_end = encode_chapter_verse(commentary.bible_verse_range.end_chapter, commentary.bible_verse_range.end_verse)
+    return [
+        str(uuid.uuid4()),
+        commentary.father_name,
+        commentary.filename,
+        commentary.append_to_author_name,
+        commentary.date,
+        commentary.bible_book_name.lower().replace(" ", ""),
+        location_start,
+        location_end,
+        commentary.txt,
+        commentary.source_url,
+        commentary.source_title
+    ]
+
+
+def generate_father_metadata(metadata_tomls: Iterable[Path]) -> Dict[str, Dict]:
     father_meta_data = {}
+    for file in metadata_tomls:
+        toml_str = file.read_text()
+        toml_obj = rtoml.load(toml_str)
+        father_meta_data[file.parent.name] = toml_obj
+    return father_meta_data
 
-    # First loop through all metadata files, build them up into default lookup table
-    total_file_count = 0
-    for file in file_list:
-        # Pseudo-Jerome/Mark 6_35-44.toml
-        short_file_name = file.relative_to(Path.cwd())
-        if short_file_name.name == 'metadata.toml':
-            toml_str = file.read_text()
-            toml_obj = rtoml.load(toml_str)
-            father_meta_data[file.parent.name] = toml_obj
-        elif file.suffix == '.toml':
-            total_file_count += 1
 
-    data_values = []
+def process_tomls_to_rows():
+    file_list = [path for path in Path.cwd().glob('**/*') if path.is_file() and path.suffix == '.toml']
+    father_meta_data = generate_father_metadata(file for file in file_list if file.name == 'metadata.toml')
 
     # Then loop through all files, load them into an object
-    current_file_count = 0
-    for file in file_list:
-        try:
-            if file.name == 'metadata.toml':
-                continue
-
-            for i in range(10):
-                if current_file_count == int(total_file_count * (0.1 * i)):
-                    print(f"{i}0% done ({current_file_count} / {total_file_count})")
-            current_file_count += 1
-
-            father_name = file.parent.name
-            file_name = file.stem
-            fn_pieces = file_name.split(" ")
-            book_name = " ".join(fn_pieces[:-1])
-
-            verse_range = string_to_verse_range(fn_pieces[-1])
-
-            location_start = encode_chapter_verse(verse_range.start_chapter, verse_range.start_verse)
-            location_end = encode_chapter_verse(verse_range.end_chapter, verse_range.end_verse)
-
-            toml_str = file.read_text(encoding='utf-8')
-            toml_obj = rtoml.load(toml_str)
-
-            for c in toml_obj['commentary']:
-                source_url = ""
-                source_title = ""
-                if 'sources' in c:
-                    source_url = c['sources'][0]['url']
-                    source_title = c['sources'][0]['title']
-
-                time = 9999999
-                if father_name in father_meta_data:
-                    time = father_meta_data[father_name]['default_year']
-                if 'time' in c:
-                    time = c['time']
-
-                append_to_author_name = c.get('append_to_author_name', '')
-
-                data_values.append([
-                    str(uuid.uuid4()),
-                    father_name,
-                    file.name,
-                    append_to_author_name,
-                    time,
-                    book_name.lower().replace(" ", ""),
-                    location_start,
-                    location_end,
-                    c['quote'],
-                    source_url,
-                    source_title
-                ])
-        except BaseException as error:
-            print("******Error in", file)
-            print("Error Reads: ", error)
-            raise
-    print(f"*Files processed: {current_file_count}")
+    commentary_data = [commentary_to_row(commentary)
+                       for toml in file_list if toml.name != 'metadata.toml'
+                       for commentary in process_toml_file(toml, father_meta_data)
+                       ]
 
     formatted_father_meta_data = [
         [
@@ -141,7 +169,7 @@ def process_toml():
     ]
     return {
         "father_meta_data": formatted_father_meta_data,
-        "commentary_data": data_values
+        "commentary_data": commentary_data
     }
 
 
@@ -202,9 +230,8 @@ def to_sqlite(toml_data: Dict, out_path: Path):
 
 
 def to_json(toml_data: Dict, out_path: Path):
-    final_data = []
-    for d in toml_data['commentary_data']:
-        final_data.append({
+    final_data = [
+        {
             "id": d[0],
             "father_name": d[1],
             "file_name": d[2],
@@ -216,9 +243,10 @@ def to_json(toml_data: Dict, out_path: Path):
             "txt": d[8],
             "source_url": d[9],
             "source_title": d[10],
-        })
-    with out_path.open('w') as f:
-        json.dump(final_data, f)
+        }
+        for d in toml_data['commentary_data']
+    ]
+    json.dump(final_data, out_path.open('w'))
 
 
 def to_csv(toml_data: Dict, out_path: Path):
@@ -245,7 +273,7 @@ def main():
     args = parse_arguments()
 
     print('Starting to Compile')
-    toml_data = process_toml()
+    toml_data = process_tomls_to_rows()
     if args.output_format == 'json':
         print("Saving into JSON...")
         to_json(toml_data, args.out)
